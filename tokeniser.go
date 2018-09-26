57623062c88b8f0fdef2f6e88c149f05a7927e26
Henry Blyth <blyth.henry@gmail.com>
2018-09-26 14:08:01 +0100

    Use stdlib for newline detection (refactor)

--- tokeniser.go
+++ tokeniser.go
@@ -1,6 +1,11 @@
 package workflowlang
 
-import "bytes"
+import (
+	"bufio"
+	"bytes"
+	"log"
+	"strings"
+)
 
 type Token struct {
 	line       int
@@ -20,61 +25,74 @@ func NewTokeniser(symbols []byte) *tokeniser {
 }
 
 var whitespace = []byte{' ', '\t'}
-var newlines = []byte{'\r', '\n'}
 
-// var defaultSymbols = []byte{'(', ')', '$', '\\', '\n', '\t'}
+// var defaultSymbols = []byte{'(', ')', '$', '\\'}
 
 func (t *tokeniser) Stream(input string) (stream []*Token) {
 	text := ""
 	space := ""
 	col := 0
-	line := 0
-	colReset := 0
-	for i, char := range input {
-		if bytes.ContainsRune(t.symbols, char) {
-			if text != "" {
-				stream = append(stream, &Token{line: line, col: col, text: text})
-				text = ""
+	line := -1
+
+	all := []string{}
+	r := bufio.NewScanner(strings.NewReader(input))
+	for r.Scan() {
+		all = append(all, r.Text())
+	}
+	if err := r.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	lastline := len(all) - 1
+	for line, linestr := range all {
+		col = 0
+		for i, char := range linestr {
+			if bytes.ContainsRune(t.symbols, char) {
+				if text != "" {
+					stream = append(stream, &Token{line: line, col: col, text: text})
+					text = ""
+				}
+				if space != "" {
+					stream = append(stream, &Token{line: line, col: col, whitespace: space})
+					space = ""
+				}
+				col = i
+				stream = append(stream, &Token{line: line, col: col, symbol: char})
+				col += 1
+			} else if bytes.ContainsRune(whitespace, char) {
+				if text != "" {
+					stream = append(stream, &Token{line: line, col: col, text: text})
+					text = ""
+					col = i
+				}
+				space = space + string(char)
+			} else {
+				if space != "" {
+					stream = append(stream, &Token{line: line, col: col, whitespace: space})
+					space = ""
+					col = i
+				}
+				text = text + string(char)
 			}
-			if space != "" {
-				stream = append(stream, &Token{line: line, col: col, whitespace: space})
-				space = ""
-			}
-			col = i - colReset
-			stream = append(stream, &Token{line: line, col: col, symbol: char})
-			col += 1
-		} else if bytes.ContainsRune(newlines, char) {
-			if text != "" {
-				stream = append(stream, &Token{line: line, col: col, text: text})
-				text = ""
-			}
-			if space != "" {
-				stream = append(stream, &Token{line: line, col: col, whitespace: space})
-				space = ""
-			}
-			col = i - colReset
-			stream = append(stream, &Token{line: line, col: col, newline: true})
-			col = 0
-			colReset = i + 1
-			line += 1
-		} else if bytes.ContainsRune(whitespace, char) {
-			if text != "" {
-				stream = append(stream, &Token{line: line, col: col, text: text})
-				text = ""
-				col = i - colReset
-			}
-			space = space + string(char)
-		} else {
-			if space != "" {
-				stream = append(stream, &Token{line: line, col: col, whitespace: space})
-				space = ""
-				col = i - colReset
-			}
-			text = text + string(char)
+		}
+		if text != "" {
+			stream = append(stream, &Token{line: line, col: col, text: text})
+			text = ""
+		}
+		if space != "" {
+			stream = append(stream, &Token{line: line, col: col, whitespace: space})
+			space = ""
+		}
+		if line != lastline {
+			stream = append(stream, &Token{line: line, col: len(linestr), newline: true})
 		}
 	}
+
 	if text != "" {
 		stream = append(stream, &Token{line: line, col: col, text: text})
+	}
+	if space != "" {
+		stream = append(stream, &Token{line: line, col: col, whitespace: space})
 	}
 	return
 }
