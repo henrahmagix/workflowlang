21a1d5fc27de2e87f9d193ea8f538c4e792172fc
Henry Blyth <blyth.henry@gmail.com>
2019-03-03 09:06:37 +0900

    Make tokeniser a bit easier to read & remove unused

--- tokeniser.go
+++ tokeniser.go
@@ -27,71 +27,67 @@ func NewTokeniser(symbols []byte) *tokeniser {
 
 // var defaultSymbols = []byte{'(', ')', '$', '\\'}
 
-func (t *tokeniser) Stream(input string) (stream []*Token) {
-	text := ""
-	space := ""
-	col := 0
-	line := -1
+func (t *tokeniser) Stream(input string) []*Token {
+	var tokens []*Token
 
-	all := []string{}
+	allLines := []string{}
 	r := bufio.NewScanner(strings.NewReader(input))
 	for r.Scan() {
-		all = append(all, r.Text())
+		allLines = append(allLines, r.Text())
 	}
 	if err := r.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	lastline := len(all) - 1
-	for line, linestr := range all {
-		col = 0
+	lastline := len(allLines) - 1
+
+	for line, linestr := range allLines {
+		var (
+			text       string
+			whitespace string
+			col        int
+		)
 		for i, char := range linestr {
 			if bytes.ContainsRune(t.symbols, char) {
 				if text != "" {
-					stream = append(stream, &Token{line: line, col: col, text: text})
+					tokens = append(tokens, &Token{line: line, col: col, text: text})
 					text = ""
 				}
-				if space != "" {
-					stream = append(stream, &Token{line: line, col: col, whitespace: space})
-					space = ""
+				if whitespace != "" {
+					tokens = append(tokens, &Token{line: line, col: col, whitespace: whitespace})
+					whitespace = ""
 				}
 				col = i
-				stream = append(stream, &Token{line: line, col: col, symbol: char})
+				tokens = append(tokens, &Token{line: line, col: col, symbol: char})
 				col += 1
 			} else if unicode.IsSpace(char) {
 				if text != "" {
-					stream = append(stream, &Token{line: line, col: col, text: text})
+					tokens = append(tokens, &Token{line: line, col: col, text: text})
 					text = ""
 					col = i
 				}
-				space = space + string(char)
+				whitespace = whitespace + string(char)
 			} else {
-				if space != "" {
-					stream = append(stream, &Token{line: line, col: col, whitespace: space})
-					space = ""
+				if whitespace != "" {
+					tokens = append(tokens, &Token{line: line, col: col, whitespace: whitespace})
+					whitespace = ""
 					col = i
 				}
 				text = text + string(char)
 			}
 		}
 		if text != "" {
-			stream = append(stream, &Token{line: line, col: col, text: text})
+			tokens = append(tokens, &Token{line: line, col: col, text: text})
 			text = ""
 		}
-		if space != "" {
-			stream = append(stream, &Token{line: line, col: col, whitespace: space})
-			space = ""
+		if whitespace != "" {
+			tokens = append(tokens, &Token{line: line, col: col, whitespace: whitespace})
+			whitespace = ""
 		}
 		if line != lastline {
-			stream = append(stream, &Token{line: line, col: len(linestr), newline: true})
+			tokens = append(tokens, &Token{line: line, col: len(linestr), newline: true})
 		}
 	}
 
-	if text != "" {
-		stream = append(stream, &Token{line: line, col: col, text: text})
-	}
-	if space != "" {
-		stream = append(stream, &Token{line: line, col: col, whitespace: space})
-	}
-	return
+	return tokens
 }
